60a9edffd3a1f93283dc96170bc8874bba82483a
Nelson Reina (@ibis-nreina) <nelson.reina@ibis-management.com>
2025-01-02 04:46:52 -0400

    added yazi config to stow

---
+++ yazi/.config/yazi/init.lua
@@ -0,0 +1,32 @@
+-- ~/.config/yazi/init.lua
+require("bookmarks"):setup({
+	last_directory = { enable = true, persist = false },
+	persist = "all",
+	desc_format = "full",
+	file_pick_mode = "hover",
+	notify = {
+		enable = true,
+		timeout = 5,
+		message = {
+			new = "New bookmark '<key>' -> '<folder>'",
+			delete = "Deleted bookmark in '<key>'",
+			delete_all = "Deleted all bookmarks",
+		},
+	},
+})
+
+require("copy-file-contents"):setup({
+	append_char = "\n",
+	notification = true,
+})
+
+require("git"):setup()
+
+require("full-border"):setup()
+
+require("starship"):setup()
+
+require("custom-shell"):setup({
+	history_path = "default",
+	save_history = true,
+})
