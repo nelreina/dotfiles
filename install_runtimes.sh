729761580c015331712a5791123d38fe753ad1ea
Nelson Reina (@ibis-nreina) <nelson.reina@ibis-management.com>
2025-01-18 02:43:07 -0400

    install unzip for deno

--- install_runtimes.sh
+++ install_runtimes.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+sudo apt-get update
+sudo apt-get install -y curl
+sudo apt-get install -y unzip
 
 curl -fsSL https://deno.land/install.sh | sh
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-
+export NVM_DIR="$HOME/.nvm"
 nvm install node
 
 node -v
