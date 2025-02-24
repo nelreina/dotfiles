891de744d8966902dff538c8316b5aacf02f012a
Nelson Reina (@ibis-nreina) <nelson.reina@ibis-management.com>
2025-02-24 04:54:43 -0400

    changed order of glone

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# First clone the repo
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
 # Update package list
 echo "Updating package list..."
 sudo apt update
@@ -29,6 +24,11 @@ rm ~/.zshrc
 rm ~/.tmux.conf
 cd zsh
 stow -t ~ remote
+# First clone the repo
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
 
 # Set Zsh as the default shell
 echo "Changing default shell to Zsh..."
