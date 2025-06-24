c0ccc829a9fc121eb856e247b0b61ef8b9fe3cf6
Nelreina <nelreina@gmail.com>
2025-06-24 17:37:40 -0400

    Create install-psycopg2.sh

---
+++ install-psycopg2.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install python3-dev libpq-dev build-essential
+pip install psycopg2 redis
+
+# Or use apt to install psycopg2 directly
+sudo apt-get install python3-psycopg2
