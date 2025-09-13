5e27690aa6dc12ae623b9c239c6733e632f1d54e
Nelson Reina (@nelreina) <nelreina@gmail.com>
2025-09-13 15:28:48 -0400

    improved mac productivity

---
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,5 @@
+-- ~/.config/nvim/lua/plugins/neo-tree.lua
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  enabled = false, -- This disables neo-tree
+}
---
+++ nvim/.config/nvim/lua/plugins/yazi.lua
@@ -0,0 +1,34 @@
+return {
+  "mikavilpas/yazi.nvim",
+  version = "*", -- use the latest stable version
+  event = "VeryLazy",
+  dependencies = {
+    { "nvim-lua/plenary.nvim", lazy = true },
+  },
+  keys = {
+    -- Choose your own keymappings
+    {
+      "<leader>e", -- This replaces the default file explorer keybind
+      mode = { "n", "v" },
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
+    },
+    {
+      "<leader>cw",
+      "<cmd>Yazi cwd<cr>",
+      desc = "Open the file manager in nvim's working directory",
+    },
+    {
+      "<c-up>",
+      "<cmd>Yazi toggle<cr>",
+      desc = "Resume the last yazi session",
+    },
+  },
+  opts = {
+    -- if you want to open yazi instead of netrw for directories
+    open_for_directories = false,
+    keymaps = {
+      show_help = "<f1>",
+    },
+  },
+}
